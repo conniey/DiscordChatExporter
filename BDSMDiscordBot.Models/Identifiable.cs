ee2c3a2eb0931001d070edfdf066d8b176691d3d
Connie <conniey@microsoft.com>
2020-10-12 19:23:29 -0700

    Adding documentation.

--- BDSMDiscordBot.Models/Identifiable.cs
+++ BDSMDiscordBot.Models/Identifiable.cs
@@ -1,7 +1,14 @@
 ﻿namespace BDSMDiscordBot.Models
 {
+    /// <summary>
+    /// An identifiable object.
+    /// </summary>
     interface Identifiable
     {
+        /// <summary>
+        /// Gets the unique identifier for the object. The identifier is not globally unique. Only unique with the same
+        /// class of objects.
+        /// </summary>
         string? Id { get; }
     }
 }
